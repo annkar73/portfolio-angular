ceb30274d7212a17d4c2ff42cc0c73f3b81fd843
Anna Karlsen <146820987+annkar73@users.noreply.github.com>
2025-02-06 13:39:47 +0100

    update eduComponent

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
   standalone: true, // Viktigt!
+  imports: [CommonModule],
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
-export class EduComponent {}
+export class EduComponent {
+  courses: string[] = [
+    'HTML och CSS',
+    'JavaScript - grundkurs',
+    'Agilt projektarbete',
+    'API-utveckling',
+    'Arbetsmetodik för utvecklare',
+    'Grafiska verktyg för gränssnittsdesign',
+    'UX Usability',
+    'Testning',
+    'JavaScript - fördjupning',
+    'LIA - Lärande i Arbete',
+    'Examensarbete',
+    'Kompetensportfölj och Entreprenörskap'
+  ];
+
+  liaText: string ='Lägg till information om lia-perioder.';
+  degreeText: string = 'Lägg till information om examensarbete';
+}
